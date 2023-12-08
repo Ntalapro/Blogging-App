0f2d19ba844152e600e7f9dd0b0dcefdf0ec1017
Ntalapro <ncosanetshepiso@gmail.com>
2023-12-08 10:45:46 +0200

    spring security

---
+++ src/main/java/com/bloggingapp/bloggingapp/security/AppSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.bloggingapp.bloggingapp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class AppSecurityConfig {
+
+
+}
